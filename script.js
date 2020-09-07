dd2c075847dddb4b6af4a33896c1e04005c27039
haeun159951 <43290846+haeun159951@users.noreply.github.com>
2020-09-06 21:44:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+/** Add any JavaScript you need to this file. */
+
+$(document).ready(function(){
+  $(".item-img").on('click', function(){
+    $(".cover").fadeIn('slow');
+    $(".popup").fadeIn('slow');
+  });
+
+  $(".popup").on('click', function(){
+    if($(event.target).is("#close")){
+        $(".cover").fadeOut('slow');    
+        $(".popup").fadeOut('slow'); 
+    }
+  });
+  
+  $('.cover').on('click', function(){
+     $(".cover").fadeOut('slow');    
+     $(".popup").fadeOut('slow'); 
+  });
+});
+
+
